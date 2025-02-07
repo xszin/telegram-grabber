abde1428c101b079cac736c1a49fc4bf8a762f26
xszin <39820405+xszin@users.noreply.github.com>
2025-02-07 15:44:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-api_id = 
-api_hash = ''
-bot_token = ''
-my_id = 
+api_id = 29674544
+api_hash = '45c1a937a510424a34c4d9b61a7d7747'
+bot_token = '6556333579:AAFpZ42kIKENKaO4tKT2zpwlftw84P3GpxE'
+my_id = 139133626
 
-technical_channel_id =   # ID технического канала
+technical_channel_id = 2137894068  # ID технического канала
 
 new_link = "http://t.me/"
 new_username = "@"
